338407a0b6b8ee4f64885b09375946892d239357
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-14 23:07:12 +0900

    Add files via upload

---
+++ fizzbuzz_proc.rb
@@ -0,0 +1,11 @@
+﻿def fizz_buzz(num)
+  re = Proc.new{|str| return str}
+	re.call('Fizz')      if num % 3 == 0
+	re.call('Buzz')      if num % 5 == 0
+	re.call('Fizz Buzz') if num % 15 == 0
+	re.call(num.to_s)
+end
+
+for i in 1..100 do
+  p fizz_buzz(i)
+end
